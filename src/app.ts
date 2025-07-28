be0d672abea684c29c5bc939ac4cd3a96e429f3c
ArifinSumsul <s.arifinoriginal@gmail.com>
2025-07-28 13:28:05 +0600

    last

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
 import { router } from "./app/routes";
 
+
+
 const app = express()
 
 
@@ -29,7 +31,6 @@ app.use(cors({
 }))
 
 app.use("/api/v1", router)
-
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Welcome to Tour Management System Backend"
@@ -41,4 +42,4 @@ app.use(globalErrorHandler)
 
 app.use(notFound)
 
-export default app
+export default app;
